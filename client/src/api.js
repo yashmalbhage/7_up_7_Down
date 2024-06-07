601e27813d62db51d3d389ecfeaac0dfaa8eafc9
snehamalbhage <snehamalbhage4@gmail.com>
2024-06-07 17:48:56 +0530

    react api

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://7-up-7-down-ovsb2o9si-yashmalbhages-projects.vercel.app/api'
+    baseURL: 'https://7-up-7-down-three.vercel.app/api'
 });
 
 export const rollDice = (bet) => api.post('/roll', { bet });
